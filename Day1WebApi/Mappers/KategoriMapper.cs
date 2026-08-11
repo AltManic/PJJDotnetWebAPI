ed2e938d4a97dad5051a953574ea8fb243a375f9
Ripas Filqadar <ripas.filqadar@kemenkeu.go.id>
2026-08-11 11:36:04 +0700

    add auto mapper

---
+++ Day1WebApi/Mappers/KategoriMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace Day1WebApi.Mappers
+{
+    public class KategoriMapper : Profile
+    {
+        public KategoriMapper()
+        {
+            CreateMap<KategoriDto, Kategori>();
+            CreateMap<Kategori, KategoriDto>();
+        }
+    }
+}
